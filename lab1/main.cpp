6a72a295386eefad8591916199001d0b8f0795a0
comprock15 <khoruzhaia@sfedu.ru>
2024-09-09 23:35:09 +0300

    Add bidirectional search

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -58,6 +58,7 @@ public:
 	{
 		// Set the start number
 		root = new state(x, 0, "$");
+		created_nodes.push(root);
 		
 		// Check if start number is already equals a desired number
 		if (x == y)
@@ -303,6 +304,7 @@ public:
 		}
 	}
 
+	// clear nodes in dynamic memory
 	void purge_nodes()
 	{
 		while (!created_nodes.empty())
@@ -316,6 +318,161 @@ public:
 	~BFS()
 	{
 		purge_nodes();
+	}
+
+	// merge x -> n and n <- y
+	void merge_paths(state* s1, state* s2)
+	{
+		state* s = s2->parent;
+		string last_op = s2->lastOp;
+
+		s2->parent = s1->parent;
+		s2->lastOp = s1->lastOp;
+		s2->depth = s1->depth;
+
+		s1 = s2;
+		s2 = s;
+		
+		while (s2 != nullptr)
+		{
+			swap(last_op, s2->lastOp);
+			s = s2->parent;
+			//last_op = s2->lastOp;
+			//s2->lastOp = s1->lastOp;
+			s2->parent = s1;
+			s2->depth = s1->depth + 1;
+			s1 = s2;
+			s2 = s;
+		}
+
+		ans = s1;
+	}
+
+	// Task 4, bidirectional search
+	bool search_bidirectional(size_t x, size_t y)
+	{
+		// Set the start number
+		root = new state(x, 0, "$");
+		created_nodes.push(root);
+		ans = new state(y, 0, "$");
+		created_nodes.push(ans);
+
+		// Check if start number is already equals a desired number
+		if (x == y)
+		{
+			ans = root;
+			return true;
+		}
+		++n_states_checked;
+
+		vector<state*> checked_nums1 = vector<state*>(y + 1, nullptr);
+		vector<state*> checked_nums2 = vector<state*>(y + 1, nullptr);
+
+		checked_nums1[x] = root;
+		checked_nums2[y] = ans;
+
+		// Queue of states to check
+		queue<state*> q1, q2;
+
+		// Queue next states
+		state* st1;
+
+		st1 = new state(x * 2, 1, "*2", root);
+		q1.push(st1);
+		created_nodes.push(st1);
+
+		st1 = new state(x + 3, 1, "+3", root);
+		q1.push(st1);
+		created_nodes.push(st1);
+
+		if (y % 2 == 0)
+		{
+			st1 = new state(y / 2, 1, "*2", ans);
+			q2.push(st1);
+			created_nodes.push(st1);
+		}
+		st1 = new state(y - 3, 1, "+3", ans);
+		q2.push(st1);
+		created_nodes.push(st1);
+
+
+		// Check all states in queue
+		while (!q1.empty() && !q2.empty())
+		{
+			// front search
+			{
+				// Get current state
+				state* st = q1.front();
+				q1.pop();
+				//cout << st->value << "\n";
+				++n_states_checked;
+
+				// Check if state is correct and hasn't been checked before
+				if (st->value > y || checked_nums1[st->value] != nullptr)
+				{
+					continue;
+				}
+
+				// Set the current number to be checked
+				checked_nums1[st->value] = st;
+
+				// If found answer
+				if (checked_nums2[st->value] != nullptr)
+				{
+					merge_paths(st, checked_nums2[st->value]);
+					return true;
+				}
+
+				// Queue next states
+				st1 = new state(st->value * 2, st->depth + 1, "*2", st);
+				q1.push(st1);
+				created_nodes.push(st1);
+
+				st1 = new state(st->value + 3, st->depth + 1, "+3", st);
+				q1.push(st1);
+				created_nodes.push(st1);
+			}
+
+			// back search
+			{
+				// Get current state
+				state* st = q2.front();
+				q2.pop();
+				//cout << st->value << "\n";
+				++n_states_checked;
+
+				// Check if state is correct and hasn't been checked before
+				if (st->value < x || checked_nums2[st->value] != nullptr)
+				{
+					continue;
+				}
+
+				// Set the current number to be checked
+				checked_nums2[st->value] = st;
+
+				// If found answer
+				if (checked_nums1[st->value] != nullptr)
+				{
+					merge_paths(checked_nums1[st->value], st);
+					return true;
+				}
+
+				// Queue next states
+				if (st->value % 2 == 0)
+				{
+					st1 = new state(st->value / 2, st->depth + 1, "*2", st);
+					q2.push(st1);
+					created_nodes.push(st1);
+				}
+
+				st1 = new state(st->value - 3, st->depth + 1, "+3", st);
+				q2.push(st1);
+				created_nodes.push(st1);
+			}
+		}
+
+		return false;
+
 	}
 };
 
@@ -339,25 +496,33 @@ void run_tests(const vector<testcase>& task, int task_n)
 
 	for (int i = 0; i < task.size(); i++)
 	{
-		t1 = chrono::steady_clock::now();
 		switch (task_n)
 		{
 		case 1:
+			t1 = chrono::steady_clock::now();
 			bfs.search(task[i].x, task[i].y);
+			t2 = chrono::steady_clock::now();
 			break;
 		case 2:
+			t1 = chrono::steady_clock::now();
 			bfs.search2(task[i].x, task[i].y);
+			t2 = chrono::steady_clock::now();
 			break;
 		case 3:
+			t1 = chrono::steady_clock::now();
 			bfs.search_reversed(task[i].x, task[i].y);
+			t2 = chrono::steady_clock::now();
+			break;
+		case 4:
+			t1 = chrono::steady_clock::now();
+			bfs.search_bidirectional(task[i].x, task[i].y);
+			t2 = chrono::steady_clock::now();
 			break;
 		default:
 			break;
 		}
-
-		t2 = chrono::steady_clock::now();
 		//cout << task[i].x << " -> " << task[i].y << "\n    ";
-		bfs.print_result(0);
+		bfs.print_result(0); // if arg = 1 then operations are printed else not
 
 		cout << "    Time: " << chrono::duration_cast<chrono::milliseconds> (t2 - t1).count() << " ms\n";
 
@@ -400,11 +565,14 @@ void solve()
 		testcase(2, 10000001, 30)
 	};
 
-	cout << "----Task2----\n";
+	cout << "\n----Task2----\n";
 	run_tests(task2, 2);
 
-	cout << "----Task3----\n";
+	cout << "\n----Task3----\n";
 	run_tests(task1, 3);
+
+	cout << "\n----Task4----\n";
+	run_tests(task1, 4);
 }
 
 int main()
