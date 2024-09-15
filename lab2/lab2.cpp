8dd0f4f37e66cd331cf8f8d84f867032c7893298
comprock15 <khoruzhaia@sfedu.ru>
2024-09-15 18:01:09 +0300

    Add BFS solution length finder

---
+++ lab2/lab2.cpp
@@ -0,0 +1,256 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+#include <unordered_set>
+#include <queue>
+
+using namespace std;
+
+enum class directions { RIGHT, DOWN, LEFT, UP };
+
+struct state
+{
+    short depth;
+    directions previous_direction;
+    string positions;
+    const state& previous_state;
+    short zero_index;
+
+    state(const string& pos, directions dir, short depth, short zero_ind, const state& prev_state) :
+        positions(pos),
+        previous_direction(dir),
+        depth(depth),
+        zero_index(zero_ind),
+        previous_state(previous_state) {};
+
+    friend ostream& operator<<(ostream& os, const state& st)
+    {
+        os << st.depth << ". ";
+
+        switch (st.previous_direction)
+        {
+        case directions::DOWN:
+            os <<"DOWN\n";
+            break;
+        case directions::UP:
+            os << "UP\n";
+            break;
+        case directions::RIGHT:
+            os << "RIGHT\n";
+            break;
+        case directions::LEFT:
+            os << "LEFT\n";
+            break;
+        default:
+            os << "Error with direction\n";
+        }
+
+        for (size_t i = 0; i < st.positions.length(); i++)
+        {
+            short num = stoi(string(1, st.positions[i]), 0, 16);
+            if (num < 10)
+                os << " ";
+
+            if (num == 0)
+                os << "  ";
+            else
+                os << num << " ";
+
+            if ((i + 1) % 4 == 0)
+                os << "\n";
+        }
+
+        return os;
+    }
+};
+
+class puzzle15
+{
+private:
+    // Size of side
+    short side_size;
+    // Count of elements
+    short size;
+    // Positions of numbers
+    string start_positions;
+    
+    int nodes_checked = 0;
+
+    //queue<state> created_states;
+
+    const string solved = "123456789ABCDEF0";
+
+    // Get numbers positions from string
+    friend istream& operator>>(istream& is, puzzle15& p15)
+    {
+        for (size_t i = 0; i < p15.size; i++)
+        {
+            is >> p15.start_positions[i];
+        }
+        return is;
+    }
+
+    // Print puzzle15 current state
+    friend ostream& operator<<(ostream& os, const puzzle15& p15)
+    {
+        for (size_t i = 0; i < p15.size; i++)
+        {
+            short num = stoi(string(1, p15.start_positions[i]), 0, 16);
+            if (num < 10)
+                os << " ";
+
+            if (num == 0)
+                os << "  ";
+            else
+                os << num << " ";
+
+            if ((i + 1) % p15.side_size == 0)
+                os << "\n";
+        }
+        return os;
+    }
+
+    inline bool is_solvable(const string& pos)
+    {
+        int inversions = 0;
+        int zero_row = 0;
+        for (size_t i = 0; i < size; ++i)
+        {
+            if (pos[i] != '0')
+            {
+                for (size_t j = i + 1; j < size; ++j)
+                {
+                    if ((pos[j] != '0') && (pos[i] > pos[j]))
+                        ++inversions;
+                }
+            }
+            else
+            {
+                zero_row = i / 4;
+            }
+        }
+
+        if (side_size % 2)
+            return inversions % 2 == 0;
+
+        return (inversions + zero_row) % 2 != 0;
+    }
+
+public:
+    puzzle15(short side_size)
+    {
+        this->side_size = side_size;
+        this->size = side_size * side_size;
+        start_positions = string(size, '0');
+    }
+    
+    // Flat print of current state of grid
+    void print()
+    {
+        cout << start_positions << "\n";
+    }
+
+    bool BFS()
+    {
+        if (!is_solvable(start_positions))
+            return false;
+
+        ++nodes_checked;
+        if (start_positions == solved)
+            return true;
+
+        unordered_set<string> checked_states;
+        checked_states.insert(start_positions);
+
+        int zero_index;
+
+        for (zero_index = 0; zero_index < size; ++zero_index)
+            if (start_positions[zero_index] == '0')
+                break;
+
+        queue<state> q;
+
+        state dummy = state(start_positions, directions::UP, (short)-1, (short)zero_index, dummy);
+        state root = state(start_positions, directions::UP, (short)0, (short)zero_index, dummy);
+
+        add_states(root, q);
+
+        while (!q.empty())
+        {
+            state st = q.front();
+            q.pop();
+
+            //cout << "prev " << st->previous_state << "\n";
+            //cout << st << "\n";
+
+            if (checked_states.find(st.positions) != checked_states.end())
+                continue;
+
+            ++nodes_checked;
+            if (st.positions == solved)
+            {
+                cout << "depth: " << st.depth << " nodes checked: " << nodes_checked << "\n";
+                return true;
+            }
+
+            checked_states.insert(st.positions);
+            add_states(st, q);
+        }
+
+        return false;
+    }
+
+    inline void add_states(const state& st, queue<state>& q)
+    {
+        if (st.zero_index - 4 >= 0)
+        {
+            string pos1 = st.positions;
+            swap(pos1[st.zero_index], pos1[st.zero_index - 4]);
+            //created_states.push(state(pos1, directions::UP, st.depth + 1, st.zero_index - 4, *st));
+            q.push(state(pos1, directions::UP, st.depth + 1, st.zero_index - 4, st));
+        }
+
+        if (st.zero_index + 4 < size)
+        {
+            string pos1 = st.positions;
+            swap(pos1[st.zero_index], pos1[st.zero_index + 4]);
+            q.push(state(pos1, directions::DOWN, st.depth + 1, st.zero_index + 4, st));
+        }
+
+        if (st.zero_index % 4 != 0)
+        {
+            string pos1 = st.positions;
+            swap(pos1[st.zero_index], pos1[st.zero_index - 1]);
+            //created_states.push(state(pos1, directions::LEFT, st.depth + 1, st.zero_index - 1, *st));
+            q.push(state(pos1, directions::LEFT, st.depth + 1, st.zero_index - 1, st));
+        }
+
+        if (st.zero_index % 4 != 3)
+        {
+            string pos1 = st.positions;
+            swap(pos1[st.zero_index], pos1[st.zero_index + 1]);
+            q.push(state(pos1, directions::RIGHT, st.depth + 1, st.zero_index + 1, st));
+        }
+    }
+};
+
+void solve()
+{
+    string s;
+    s = "12345678A0BE9FCD";
+
+    puzzle15 p15 = puzzle15(4);
+    istringstream iss(s);
+
+    iss >> p15;
+    //cin >> p15;
+    cout << p15;
+
+    p15.BFS();
+}
+
+int main()
+{
+    solve();
+}
+
