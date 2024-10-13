8d7d55f1ebe8bfbfe331da47c98d7f39025e6972
comprock15 <khoruzhaia@sfedu.ru>
2024-10-13 18:25:55 +0300

    Add moves parsing

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -6,10 +6,13 @@ const _int8 BOARD_SIZE = SIDE_SIZE * SIDE_SIZE;
 
 class game
 {
-	std::array<__int8, SIDE_SIZE* SIDE_SIZE> board;
+	std::array<__int8, BOARD_SIZE> board;
+	bool bot_turn = false;
+	__int8 bot_color = -1;
+	__int8 opponent_color = 1;
 
 public:
-	game()
+	game(bool turn) : bot_turn(turn)
 	{
 		for (__int8 i = 0; i < BOARD_SIZE; ++i)
 			board[i] = 0;
@@ -18,6 +21,30 @@ public:
 		board[28] = 1;
 		board[35] = 1;
 		board[36] = -1;
+
+		if (bot_turn)
+		{
+			bot_color = 1;
+			opponent_color = -1;
+		}
+	}
+
+	void start_game()
+	{
+		print_board();
+		while (!game_over())
+		{
+			if (bot_turn)
+			{
+				make_move();
+			}
+			else
+			{
+				parse_move();
+			}
+			print_board();
+			switch_turn();
+		}
 	}
 
 	void print_board()
@@ -46,11 +73,66 @@ public:
 			std::cout << "\n   -------------------------------------------------\n";
 		}
 	}
+
+	inline void parse_move()
+	{
+		char column, row;
+		std::cin >> column >> row;
+		board[SIDE_SIZE * (row - '1') + (column - 'a')] = opponent_color;
+	}
+
+	void make_move()
+	{
+		for (__int8 i = 0; i < BOARD_SIZE; ++i)
+			if (!board[i])
+			{
+				board[i] = bot_color;
+				std::cerr << (char)('a' + i % SIDE_SIZE) << i / SIDE_SIZE + 1 << "\n";
+				return;
+			}
+	}
+
+	inline void switch_turn()
+	{
+		bot_turn = !bot_turn;
+	}
+
+	inline bool game_over()
+	{
+		for (__int8 i = 0; i < BOARD_SIZE; ++i)
+			if (!board[i])
+				return false;
+		return true;
+	}
+
+	__int8 game_result()
+	{
+		short bot_points = 0;
+		short opponent_points = 0;
+		for (__int8 i = 0; i < BOARD_SIZE; ++i)
+		{
+			if (board[i] == bot_color)
+				++bot_points;
+			else if (board[i] == opponent_color)
+				++opponent_points;
+		}
+
+		std::cout << "bot points: " << bot_points << "\n" <<
+			"opponent points: " << opponent_points << "\n";
+		if (bot_points > opponent_points)
+			return 0;
+		else if (bot_points < opponent_points)
+			return 3;
+		return 4;
+	}
 };
 
 int main(int argc, char* argv[])
 {
-	game g;
+	// check if bot plays first or second
+	game g(*argv[argc - 1] == '0');
 
-	g.print_board();
+	g.start_game();
+
+	return g.game_result();
 }
