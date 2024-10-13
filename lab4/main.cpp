c428488e771a1b5729fc7d9bc7a25f6b4f8f66a8
comprock15 <khoruzhaia@sfedu.ru>
2024-10-14 00:03:27 +0300

    Add moves by rules

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -4,12 +4,184 @@
 const __int8 SIDE_SIZE = 8;
 const _int8 BOARD_SIZE = SIDE_SIZE * SIDE_SIZE;
 
+enum regions {a1=99, b1=-8, c1=8, d1=6, b2=-24, c2=-4, d2=-3, c3=7, d3=4, d4=0};
+enum directions {up, upright, right, downright, down, downleft, left, upleft};
+
 class game
 {
 	std::array<__int8, BOARD_SIZE> board;
+	std::array<__int8, BOARD_SIZE> weights = {
+		regions::a1, regions::b1, regions::c1, regions::d1, regions::d1, regions::c1, regions::b1, regions::a1,
+		regions::b1, regions::b2, regions::c2, regions::d2, regions::d2, regions::c2, regions::b2, regions::b1,
+		regions::c1, regions::c2, regions::c3, regions::d3, regions::d3, regions::c3, regions::c2, regions::c1,
+		regions::d1, regions::d2, regions::d3, regions::d4, regions::d4, regions::d3, regions::d2, regions::d1,
+		regions::d1, regions::d2, regions::d3, regions::d4, regions::d4, regions::d3, regions::d2, regions::d1,
+		regions::c1, regions::c2, regions::c3, regions::d3, regions::d3, regions::c3, regions::c2, regions::c1,
+		regions::b1, regions::b2, regions::c2, regions::d2, regions::d2, regions::c2, regions::b2, regions::b1,
+		regions::a1, regions::b1, regions::c1, regions::d1, regions::d1, regions::c1, regions::b1, regions::a1
+	};
 	bool bot_turn = false;
 	__int8 bot_color = -1;
 	__int8 opponent_color = 1;
+	bool bot_has_moves = true;
+	bool opponent_has_moves = true;
+
+	__int8 next_cell(__int8 i, directions dir)
+	{
+		switch (dir)
+		{
+		case up:
+			i -= SIDE_SIZE;
+			break;
+		case upright:
+			i -= SIDE_SIZE - 1;
+			break;
+		case right:
+			i += 1;
+			break;
+		case downright:
+			i += SIDE_SIZE + 1;
+			break;
+		case down:
+			i += SIDE_SIZE;
+			break;
+		case downleft:
+			i += SIDE_SIZE - 1;
+			break;
+		case left:
+			i -= 1;
+			break;
+		case upleft:
+			i -= SIDE_SIZE + 1;
+			break;
+		default:
+			break;
+		}
+		return i;
+	}
+
+	bool check_bounds_ok(__int8 pos, directions dir)
+	{
+		return ((dir == directions::upleft || dir == directions::up || dir == directions::upright) && (pos / SIDE_SIZE != 0)) ||
+			((dir == directions::upright || dir == directions::right || dir == directions::downright) && ((pos + 1) % SIDE_SIZE != 0)) ||
+			((dir == directions::upleft || dir == directions::left || dir == directions::upright) && (pos % SIDE_SIZE != 0)) ||
+			((dir == directions::downleft || dir == directions::down || dir == directions::downright) && (pos / SIDE_SIZE < SIDE_SIZE - 1));
+	}
+
+	bool can_make_move(__int8 pos, __int8 current_color)
+	{
+		for (directions dir = directions::up; dir < 8; dir = (directions)(dir + 1))
+		{
+			if (!check_bounds_ok(pos, (directions)dir))
+				continue;
+
+			__int8 pos1 = next_cell(pos, (directions)dir);
+
+			if (board[pos1] != -current_color)
+				continue;
+
+			while (board[pos1] == -current_color)
+			{
+				if (check_bounds_ok(pos1, (directions)dir))
+					pos1 = next_cell(pos1, (directions)dir);
+				else
+					break;
+			}
+
+			if (board[pos1] == current_color)
+				return true;
+		}
+
+		return false;
+	}
+
+	void repaint_cells(__int8 pos, __int8 current_color)
+	{
+		for (directions dir = directions::up; dir < 8; dir = (directions)(dir + 1))
+		{
+			if (!check_bounds_ok(pos, (directions)dir))
+				continue;
+
+			__int8 pos1 = next_cell(pos, (directions)dir);
+
+			if (board[pos1] != -current_color)
+				continue;
+
+			__int8 points = 0;
+			while (board[pos1] == -current_color)
+			{
+				++points;
+				if (check_bounds_ok(pos1, (directions)dir))
+					pos1 = next_cell(pos1, (directions)dir);
+				else
+					break;
+			}
+			
+			if (board[pos1] == current_color)
+			{
+				pos1 = pos;
+				//std::cout << +points << " cells to repaint\n";
+				while (points--)
+				{
+					pos1 = next_cell(pos1, (directions)dir);
+					board[pos1] = current_color;
+					//std::cout << +pos1 << "\n";
+				}
+			}
+		}
+	}
+
+	void make_move()
+	{
+		bot_has_moves = true;
+		for (__int8 i = 0; i < BOARD_SIZE; ++i)
+			if (!board[i] && can_make_move(i, bot_color))
+			{
+				board[i] = bot_color;
+				std::cerr << (char)('a' + i % SIDE_SIZE) << i / SIDE_SIZE + 1 << "\n";
+				std::cout << "bot's move: " << (char)('a' + i % SIDE_SIZE) << i / SIDE_SIZE + 1 << "\n";
+				repaint_cells(i, bot_color);
+				return;
+			}
+		std::cout << "bot doesn't have any moves\n";
+		bot_has_moves = false;
+	}
+
+	void parse_move()
+	{
+		opponent_has_moves = false;
+		for (__int8 i = 0; i < BOARD_SIZE; ++i)
+			if (!board[i] && can_make_move(i, opponent_color))
+			{
+				opponent_has_moves = true;
+				break;
+			}
+		if (opponent_has_moves)
+		{
+			char column, row;
+			std::cin >> column >> row;
+			board[SIDE_SIZE * (row - '1') + (column - 'a')] = opponent_color;
+			repaint_cells(SIDE_SIZE * (row - '1') + (column - 'a'), opponent_color);
+			std::cout << "opponent's move: " << column << row << "\n";
+		}
+		else
+		{
+			std::cout << "opponent doesn't have any moves\n";
+		}
+	}
+
+	void switch_turn()
+	{
+		bot_turn = !bot_turn;
+	}
+
+	inline bool game_over()
+	{
+		/*for (__int8 i = 0; i < BOARD_SIZE; ++i)
+			if (!board[i])
+				return false;*/
+		return !(bot_has_moves || opponent_has_moves);
+	}
 
 public:
 	game(bool turn) : bot_turn(turn)
@@ -74,37 +246,6 @@ public:
 		}
 	}
 
-	inline void parse_move()
-	{
-		char column, row;
-		std::cin >> column >> row;
-		board[SIDE_SIZE * (row - '1') + (column - 'a')] = opponent_color;
-	}
-
-	void make_move()
-	{
-		for (__int8 i = 0; i < BOARD_SIZE; ++i)
-			if (!board[i])
-			{
-				board[i] = bot_color;
-				std::cerr << (char)('a' + i % SIDE_SIZE) << i / SIDE_SIZE + 1 << "\n";
-				return;
-			}
-	}
-
-	inline void switch_turn()
-	{
-		bot_turn = !bot_turn;
-	}
-
-	inline bool game_over()
-	{
-		for (__int8 i = 0; i < BOARD_SIZE; ++i)
-			if (!board[i])
-				return false;
-		return true;
-	}
-
 	__int8 game_result()
 	{
 		short bot_points = 0;
@@ -117,8 +258,8 @@ public:
 				++opponent_points;
 		}
 
-		std::cout << "bot points: " << bot_points << "\n" <<
-			"opponent points: " << opponent_points << "\n";
+		std::cout << "bot's points: " << bot_points << "\n" <<
+			"opponent's points: " << opponent_points << "\n";
 		if (bot_points > opponent_points)
 			return 0;
 		else if (bot_points < opponent_points)
