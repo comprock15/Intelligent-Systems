40a37f098928e182ed9ddc5696dc831b90ea2156
comprock15 <khoruzhaia@sfedu.ru>
2024-12-09 22:51:13 +0300

    Add actiivation and loss functions

--- lab7/NeuralNetwork1/StudentNetwork.cs
+++ lab7/NeuralNetwork1/StudentNetwork.cs
@@ -9,8 +9,14 @@ namespace NeuralNetwork1
         private List<List<List<double>>> weights; // [слой][нейрон-адресат][нейрон-источник]
         private List<List<double>> layers; // [слой][нейрон]
 
+        private Func<double, double, double> activationFunction; // Функция активации
+        private Func<double[], double[], double> lossFunction; // Функция потерь
+
         public StudentNetwork(int[] structure)
         {
+            activationFunction = Sigmoid;
+            lossFunction = MSE;
+
             // Добавление нейрончиков
             layers = new List<List<double>>(structure.Length);
             for (int layer = 0; layer < structure.Length; ++layer)
@@ -49,6 +55,26 @@ namespace NeuralNetwork1
         protected override double[] Compute(double[] input)
         {
             throw new NotImplementedException();
+        }
+
+        // Сигмоидная функция активации
+        public double Sigmoid(double s, double alpha = 1) => 1.0 / (1 + Math.Exp(-2 * alpha * s));
+
+        // Среднеквадратичная ошибка
+        public double MSE(double[] predicted, double[] target)
+        {
+#if DEBUG
+            if (predicted.Length != target.Length)
+                throw new ArgumentException("WTF!?? Я не могу такое посчитать, длины массивов не совпадают!");
+#endif
+                double res = 0;
+
+            for (int i = 0; i < predicted.Length; ++i)
+            {
+                res += Math.Pow(target[i] - predicted[i], 2);
+            }
+
+            return res / predicted.Length;
         }
     }
 }
